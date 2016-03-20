c709acf5806df23f7f8f692da1b38d0259b5f0fc
Roxanne <cwillenbockel@aol.com>
2016-03-20 21:00:13 +0100

    V 2.01b

--- SandrahNPC/ToB/SanRTFextends.d
+++ SandrahNPC/ToB/SanRTFextends.d
@@ -4,7 +4,7 @@ IF~ Global("SanBeginRtF","GLOBAL",2)~ THEN Pellig StartRtFQ1
 DO~SetGlobal("SanBeginRtF","GLOBAL",3)~
 ==CVSAN25J~ Greetings to you both. We were already expecting your return eagerly. What news do you carry?~
 ==Solar~ We have come both together to report as things develop most disturbingly.~
-==Pellig~ The decade of peace between Shauhana's orcs and Amn seems to be in peril due to machinations that originate right here at Celestial...~
+==Pellig~ The decade of peace between Shauhana's orcs and Amn seems to be in peril due to machinations that originate right here at Celestia...~
 END
 ++~Decade?? We are here just mere weeks...~ + StartRtFQ2
 
@@ -12,10 +12,10 @@ CHAIN
 IF~~ THEN Pellig StartRtFQ2
 ~You have still not adjusted yourself to the different movement of time up here and on the prime...yes it has been a decade since you left Toril.~
 ==Solar~ With a good chance to return there pretty soon.~
-==Pellig~The rumours that Cyric has now the former exile Jon Irenicus and his minions from the Abyss in his service seem to be correct, Mystra. He and his agents have chosen the prime as their playground to act against the achievements you two have just accomplished in the Realms.~
+==Pellig~The rumours that Cyric has now the former exile Jon Irenicus and his minions from the Abyss in his service seem to be correct, Mystra. The Prince of Lies and his agents have chosen the prime as their playground to act against the achievements you two have just accomplished in the Realms.~
 ==CVSAN25J~They want to stir a war between the nations again, Pelligram? ~
 ==Pellig~It all points to that but it can only be investigated at the scene of the crime itself.~
-==CVSAN25J~It seems almost like history repeats itself again and again. (Sigh) If our foes act through the primary plane against us, it would be there that we need to face them.~
+==CVSAN25J~It seems almost like history repeats itself again and again. (Sigh) If our foe act through the primary plane against us, it would be there that we need to face them.~
 ==Solar~ This is AO's verdict as well that I am sent to give you and <CHARNAME>. It is required that you meet the assault in your avatars on Faerun and solve the problem there. The Overgod will not allow the conflict taken to other planes and causing unlimited destruction there.~
 ==CVSAN25J~What does that imply?~
 ==Pellig~We may all revert to our avatars we used in our splendid adventures before and return to the prime. Solar will be sent to take us back to here only when we accomplished our mission.~
@@ -42,7 +42,7 @@ IF~~THEN Solar StartRtFQS3
 
 APPEND Solar
 IF WEIGHT #-3 ~Global("SanRaiseMys","GLOBAL",9)~THEN BEGIN NotYet
-SAY~Here the new adventure will begin once it has been developped and installed.~
+SAY~Here the new adventure will begin once it has been installed.~
 IF~~THEN DO~SetGlobal("SanTravel","GLOBAL",0)
 SetGlobal("SanRaiseMys","GLOBAL",10)
 StartCutSceneMode()
