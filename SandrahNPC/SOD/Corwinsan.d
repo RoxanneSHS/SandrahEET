a3ffd232b027fb4de8af7a0e38f51248903edb5c
Roxanne <cwillenbockel@aol.com>
2016-06-04 23:05:17 +0200

    V 2.01b

---
+++ SandrahNPC/SOD/Corwinsan.d
@@ -0,0 +1,10 @@
+CHAIN
+IF~Global("SanCorwin","Global",3)~THEN BDCORWIJ Corwiheal1
+~In times like these it is good to have comrads who can repair and not only destroy.~
+DO~SetGlobal("SanCorwin","Global",4)~
+==CVSandrJ~Travelling with <CHARNAME> has required both my talents to a large extend.~
+==BDCORWIJ~I have seen that you can be quite destructive in our battles. I wonder if this is why <PRO_HESHE> has chosen your companionship rather than due to your healing skills.~
+==CVSandrJ~Neither of those, our common adventures and relationship started before any of us became aware of <PRO_HISHER> heritage or quest. This is the background of your inquiry, if i am not mistaken, the bhaal blood.~
+==BDCORWIJ~It seems to impact you little - well, that is not expressed correctly...you are concerned about it obviously. But not so much because of what it does to <CHARNAME>, you are bothered more about what people see in <PRO_HIMHER> because of it.~
+==CVSandrJ~I think that I know <CHARNAME> well enough to have an idea of what we - I mean, <PRO_HESHE> - will do with that *potential*. I do not know all those people and powers who now take an interest in the bhaalspawns. It is hard to protect a friend against such unknown threats.~
+==BDCORWIJ~You make it sound like <CHARNAME> may be more in danger because of this heritage than others are endangered by <PRO_HIMHER>. And interesting point of view.~EXIT
