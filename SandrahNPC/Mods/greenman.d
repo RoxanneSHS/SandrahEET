51d78cab81c462c583f80016c79c14e0fb6a324a
Roxanne <cwillenbockel@aol.com>
2016-08-08 22:39:22 +0200

    V 2.02b

--- SandrahNPC/Mods/greenman.d
+++ SandrahNPC/Mods/greenman.d
@@ -1,8 +1,8 @@
 INTERJECT_COPY_TRANS Portal 0 GreenMan
 == CVSandrJ IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ All yours, Imoen!~
-== BIMOEN2 IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ He? Whatcha talkin', sweetie - OH NO, you false little witch...~
+== BIMOEN IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ He? Whatcha talkin', sweetie - OH NO, you false little witch...~
 == CVSandrJ IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ I stand to my word of honor I gave to you, now it is your turn.~
-== BIMOEN2 IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ How could I been so dumb n' trustin', little Miss Clever, uuh.~
+== BIMOEN IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ How could I been so dumb n' trustin', little Miss Clever, uuh.~
 == CVSandrJ IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~  Just think of what the fashion addicts will say, green and pink, the event of the season...~
 == Portal IF ~ InParty("CVSandr") InParty("Imoen2") ~ THEN ~ Why do people always think that I am weird. Your company is far more of a curiosity than I am. A bit scary those two, do not let them run around unattended.~
 END
