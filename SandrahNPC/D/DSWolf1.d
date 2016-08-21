49295b853d9e5b18bf9e9eb6135496af4630fbbd
Roxanne <cwillenbockel@aol.com>
2016-08-21 11:28:20 +0200

    V 2.02b

--- SandrahNPC/D/DSWolf1.d
+++ SandrahNPC/D/DSWolf1.d
@@ -2,12 +2,12 @@ BEGIN DSWolf1
 
 CHAIN
 IF~Global("PellBubba","GLOBAL",1) ~THEN DSWolf1 Clarify
-~(Madadh n'Sgàil slowly approaches Pelligram drawn to her by curiosity while at the same time taking heed of her enourmous pranks.)~
+~(Madadh n'Sgail slowly approaches Pelligram drawn to her by curiosity while at the same time taking heed of her enourmous pranks.)~
 DO~SetGlobal("PellBubba","GLOBAL",2)~
 ==Pellig~ (The cat pretends to ignore what is going on thus encouraging the shadow wolf.)~
-== DSWolf1~ (Madadh n'Sgàil is inhaling the scent of the large cat...)~
+== DSWolf1~ (Madadh n'Sgail is inhaling the scent of the large cat...)~
 ==Haiass~ Growl...Huuuh...~
-=~(Your faithful wolf companion darts to a position between Pelligram and Madadh n'Sgàil showing his teeth to his oponent.)~
+=~(Your faithful wolf companion darts to a position between Pelligram and Madadh n'Sgail showing his teeth to his oponent.)~
 ==Pellig~ Purr...~
 == DSWolf1~ (Slowly the shadow wolf moves out of Haiass' imminent reach, suddenly interested in some object on the other side of the camp that you cannot see - if it exists at all.)~
 ==Haiass~ (Your wolf circles once around Pelligram before he lays himself down with a satisfied grunt.)~
