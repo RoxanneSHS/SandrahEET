04a41fa09813de7fa3a10755196338ba54786027
Roxanne <cwillenbockel@aol.com>
2016-11-28 20:09:03 +0100

    V 2.03b

--- SandrahNPC/Mods/NtDexpbzomb.d
+++ SandrahNPC/Mods/NtDexpbzomb.d
@@ -9,11 +9,11 @@ END
   
 IF~~THEN BEGIN 23
 SAY~ You really fail to see the high arts of necromancy...but you will learn right now.~
-IF ~~ THEN DO ~CreateCreature("NTDEATHK",[1553.3284],10) SetGlobal("DexterWrong","GLOBAL",1) SetGlobal("NTotSCSpawn","BG0400",2) ~ EXIT
+IF ~~ THEN DO ~CreateCreature("CVZLord",[1553.3284],10) SetGlobal("DexterWrong","GLOBAL",1) SetGlobal("NTotSCSpawn","BG0400",2) ~ EXIT
 END
 
 IF WEIGHT #-2~AreaCheck("BG0400") GlobalGT("DexterWrong","GLOBAL",1)~ THEN BEGIN 24
   SAY ~Eeeeh, girls, isn't a high time to change the branch? Shouldn't we become the exclusive deliverers of intensive feelings for instance...~
-  IF ~~ THEN DO ~CreateCreature("NTROACH",[1553.3284],10)~ EXIT
+  IF ~~ THEN DO ~Enemy()~ EXIT
 END
 END
