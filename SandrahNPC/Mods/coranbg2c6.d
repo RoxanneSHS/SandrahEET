cd263aaef7e6dd74a7a865567b2b095db3f8f9b0
Roxanne <cwillenbockel@aol.com>
2016-12-24 22:49:07 +0100

    V 2.03b

--- SandrahNPC/Mods/coranbg2c6.d
+++ SandrahNPC/Mods/coranbg2c6.d
@@ -9,37 +9,5 @@ INTERJECT_COPY_TRANS C6Coran 6 SanMeetCorBG2a
 END
 
 
-//Meet Coran again
-CHAIN
-IF~~THEN CVSandrJ SanCorRetBG2HadBab
-~(Laughs.) You are really badly in need of some comfort if you even crawl to me once again. I tell you what, my sweet thief, let us celebrate as Safana's memory has deserved it, both knowing it will be the last time between us ever.~
-== C6CORAN ~ You will not regret it, kitten...though it may well be that I will regret it, tasting your sweetness once again and knowing it will be never again.~
-== CVSandrJ~ Now, do not complain about what you will not have but enjoy what you will receive tonight.~
-DO~ StartCutSceneMode() Wait(2) FadeToColor([30.0],0) Wait(4) FadeFromColor([30.0],0) Wait(2) EndCutSceneMode() ~ EXIT
-
-
-CHAIN
-IF~~THEN CVSandrJ SanCorRetBG2HadNotBab
-~(Laughs.) You are really badly in need of some comfort if you even crawl to me, Coran. I tell you what, my sweet thief, let us celebrate as Safana's memory has deserved it, both knowing it will be one time only between us ever.~
-== C6CORAN ~ You will not regret it, kitten...though it may well be that I will regret it, tasting your sweetness once and knowing it will be never again.~
-== CVSandrJ~ Now, do not complain about what you will not have but enjoy what you will receive tonight.~
-DO~ StartCutSceneMode() Wait(2) FadeToColor([30.0],0) Wait(4) FadeFromColor([30.0],0) Wait(2) EndCutSceneMode() ~ EXIT
-
-EXTEND_TOP C6CORAN 51 #1
-IF~ InParty("CVSandr") GlobalGT("SanCorbabe","GLOBAL",1)~ THEN GOTO 222
-IF~ InParty("CVSandr") GlobalLT("SanCorbabe","GLOBAL",1)~ THEN GOTO 223
-END
-
-APPEND C6CORAN
-IF~~THEN BEGIN 222
-SAY ~So, my sweet kitten, here is your humble thief once again kneeling before you, my goddess, my healer, in need and begging for your forgiveness.~ 
-IF~~ THEN EXTERN CVSandrJ SanCorRetBG2HadBab
-END
-
-IF~~THEN BEGIN 223
-SAY ~So, my sweet kitten, here is a humble thief kneeling before you, my goddess, my healer, in need and begging for the kindness of your heart.~
-IF~~ THEN EXTERN CVSandrJ SanCorRetBG2HadNotBab
-END
-END
 
 
