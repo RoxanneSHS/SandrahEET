a38a452d81b4c28f954164dfbeedf9a6b3ff7900
Roxanne <cwillenbockel@aol.com>
2016-12-26 13:06:10 +0100

    V 2.03b

--- SandrahNPC/D/CVSandr.d
+++ SandrahNPC/D/CVSandr.d
@@ -3,26 +3,26 @@ BEGIN CVSANDR
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN FirstJoin
 SAY ~<CHARNAME>! I am too late (Looks down at Gorion's body with horror). You desperately need help.~
 = ~You need MY help.~
-= ~<CHARNAME>, you may call me Sandrah.~
-IF ~~ THEN REPLY ~Sandrah, I'm so glad to meet you. What is going on? Will you help me? ~ GOTO SanJoin
+= ~<CHARNAME>, my name is Sandrah.~
+IF ~~ THEN REPLY ~Sandrah, glad to meet you. What is going on? Will you help me? ~ GOTO SanJoin
 IF ~ PartyHasItem("GIFTOFM")~ THEN REPLY ~An unknown cleric girl was in Candlekeep yesterday and left this magical sword for *a hero to be*. Was that unknown cleric maybe you, Sandrah? ~ GOTO GiftofM
 IF ~~ THEN REPLY ~Sandrah, can I really trust you? ~ GOTO Trust
 END
 
 IF ~~ THEN BEGIN SanJoin
-SAY ~We are comrades in arms and partners from now on. You will not regret it, <CHARNAME>. ~
+SAY ~We can be comrades in arms and partners from now on. You will not regret it, <CHARNAME>. ~
 IF ~~ THEN REPLY ~Welcome, Sandrah. ~ DO~ IncrementGlobal("Sanpoints","GLOBAL",2)~GOTO IntroImo1
 END
 
 IF ~~ THEN BEGIN GiftofM
 SAY ~Yes, this sword is a gift of my goddess Mystra. I was afraid I may miss you before the danger arrived, so I left it with a watcher to make sure you would not be totally defenceless. ~
-IF ~~ THEN REPLY ~So you already tried to help me once, and now you are here yourself to help me again. You have proven your trustworthiness to me, my friend, I will be honored if you join me. ~ GOTO SanJoin
+IF ~~ THEN REPLY ~So you already tried to help me once, and now you are here yourself to help me again. You have proven your trustworthiness to me, I will be honored if you join me. ~ GOTO SanJoin
 IF ~~ THEN REPLY ~Even then, what would make you do such a thing? What are your motives? I am not sure whether I can trust a total stranger that provides me with an enourmous gift. ~ GOTO Trust
 END
 
 IF ~~ THEN BEGIN Trust
-SAY ~You CAN and you MUST. There is no other way. What are your other options, <CHARNAME>? ~
-IF ~~ THEN REPLY ~ Well, I think I must trust someone or I'm lost. Something tells me you are sent to help me, Sandrah, come with me.~ GOTO SanJoin
+SAY ~You CAN and you MUST. What are your other options, <CHARNAME>? ~
+IF ~~ THEN REPLY ~ Well, I think I must trust someone or I'm lost. Something tells me you are here to help me, Sandrah, come with me.~ GOTO SanJoin
 IF ~~ THEN REPLY ~ I can trust no one out here. I'm sorry. Maybe another day? ~ DO~ IncrementGlobal("Sanpoints","GLOBAL",-1)~GOTO AnotherDay
 END
 
@@ -32,7 +32,7 @@ IF ~~ THEN DO ~ AddJournalEntry(@12,INFO) ~ EXIT
 END
 
 IF ~~ IntroImo1
-SAY ~ You will need friends by your side if you want to survive on the path that is waiting for you out there. ~
+SAY ~ You will need friends by your side if you want to survive on the path that is waiting for you. ~
 IF ~ InParty("imoen2") ~ THEN REPLY ~ Imoen is here with me. She is like a sister to me. ~ DO~ IncrementGlobal("Sanpoints","GLOBAL",1)~GOTO IntroImo2b
 IF ~ !InParty("imoen2")~ THEN REPLY ~ I was alone until now... ~ GOTO WhereIsImo
 END
@@ -50,7 +50,7 @@ IF ~~ THEN REPLY ~ I could not reject her help, she has been with me as long as 
 END
 
 IF ~~ IntroImo3
-SAY ~ You are loyal to your friends, <CHARNAME>. We will stand to you when you need help and care. And you NEED help! ~ IF ~~ THEN DO ~ SetGlobal("SandrahJoined","GLOBAL",1) JoinParty()~ EXTERN BIMOEN IntroImoN
+SAY ~ You are loyal to your friends, <CHARNAME>. We will stand to you when you need help and care. And you will NEED help! ~ IF ~~ THEN DO ~ SetGlobal("SandrahJoined","GLOBAL",1) JoinParty()~ EXTERN BIMOEN IntroImoN
 END
 
 IF ~~ WhereIsImo
@@ -63,7 +63,7 @@ END
 IF ~~ LovelyFool1
 SAY ~ She is much stronger than you may think, <CHARNAME>.You must be careful to keep the friends you still have. ~
 = ~ Let us return immediately to her and ask her to join us. She needs you as much as you need her.~
-= ~ You can take care of her much better if she is with you than if she is out there alone. ~
+= ~ You can take care of her much better if she is with you than if she is out here alone. ~
 IF ~~ THEN REPLY ~ Hm, I think you are right, Sandrah. It was foolish of me to separate from her. We will go back and search for her. ~ DO~ IncrementGlobal("Sanpoints","GLOBAL",1)~GOTO LovelyFool2
 IF ~~ THEN REPLY ~ But now I have you by my side. If she is as strong as you say, she will be able to manage her own way. Surely we shall meet her again. ~ GOTO LovelyFool2
 END
@@ -82,7 +82,7 @@ END
 
 IF ~NumTimesTalkedToGT(0) ~ THEN BEGIN SecondChance
 SAY ~I see you have returned, <CHARNAME>. ~
-IF ~~ THEN REPLY ~ Well, I think I must trust someone or I'm lost. Something tells me you are sent to help me, Sandrah, come with me.~ GOTO SanJoin
+IF ~~ THEN REPLY ~ Well, I think I must trust someone or I'm lost. Something tells me you are here to help me, Sandrah, come with me.~ GOTO SanJoin
 END
 
 APPEND BIMOEN
