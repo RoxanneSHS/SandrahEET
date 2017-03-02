4cccacb8aaa97f4eebeea6856768b78df7f52e40
Roxanne <cwillenbockel@aol.com>
2017-03-02 20:07:46 +0100

    V 2.04

--- SandrahNPC/ToB/Irenicus25San.d
+++ SandrahNPC/ToB/Irenicus25San.d
@@ -263,8 +263,8 @@ INTERJECT Jonel25J 304  SanDealSoul1
 INTERJECT Jonel25J 305  SanDealSoul2
 ==Jonel25J IF~InParty("CVSandr") GlobalGT("JonRedemptioncounter","GLOBAL",10)~THEN~But - maybe there are more perspectives than you can see, celestial moth.~END CVSan25J SanDealSoul
 
-INTERJECT Finsol01 65 SanDealSoul3
-==Jonel25J IF~InParty("CVSandr") ~THEN~But - maybe there are more perspectives than you can see, celestial moth.~END CVSan25J SanDealSoul
+INTERJECT Jonel25J 322 SanDealSoul3
+==CVSan25J IF~InParty("CVSandr") ~THEN~The possibilities a new goddess may offer?~END CVSan25J SanDealSoul
 
 CHAIN
 IF~~THEN CVSan25J SanDealSoul
