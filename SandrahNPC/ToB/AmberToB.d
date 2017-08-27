e8f718939c210cf245407dc3390cc293c008ea5e
Roxanne <cwillenbockel@aol.com>
2017-08-27 13:20:28 +0200

    Added Amber ToB crosssmod

---
+++ SandrahNPC/ToB/AmberToB.d
@@ -0,0 +1,19 @@
+CHAIN
+IF~Global("M#Ambparent","Global",4)~THEN BM#Amb25 AmbCadder
+~I know that look, Sandrah, spit it out already.~
+DO~SetGlobal("M#Ambparent","Global",5)~
+==BSandr25~You want to hear more about the man Cadderly. I know a little bit, if you are interested.~
+==BM#Amb25~You know I'm not. Definitely not. What do I care for those who never cared for me!~
+==BSandr25~I know that you feel that way...~
+==BM#Amb25~You know nothing, absolutely nothing. You were born out of love from parents that wanted just you and love you and everything. Hey, it's not your fault, really, it's wonderful - for you.~
+==BSandr25~This is not about me, Amber. And sure you are right, I know NOT how you feel about it and will not pretend I do.~
+=~I will just stand here mumbling what knowledge I have and not care about who listens or not.~
+==BM#Amb25~Well, and I'll walk away and not listen if I don't care to hear anything...~
+==BSandr25~Cadderly, the priest of Oghma, is reported to have a summoning chamber in the temple he build, the Spirit Soaring. There he performed rituals, dangerous rituals, to summon demons for his studies.~
+==BM#Amb25~(Amber has not walked off too far - you know her pointed ears hear every of Sandrah's words.)~
+==BSandr25~Obviously one day he must have summoned a succubus. Succubi delight in manipulating the desires of mortals and tempting them into acts that they would normally avoid. This one most likely seduced the inexperienced Caddely for her pleasure. The *victim* may not ever have become aware of the fruit of his experience.~
+=~The succubus just dropped that unexpected result of her adventure with the mortals later on.~
+==BM#Amb25~(Sigh) Maybe or maybe not. Must I care if my *father* even knows of me or not. He may be a seeker of truth, but I'm not sure if any of us gains from the insight I could provide to him.~
+==BSandr25~Take your time to consider, Amber. Do only what you feel like you want to do. I agree with you, there is no obligation for you to deal with your heritage at all.~
+==BM#Amb25~That bottle of red wine I saw you puchase recently, is it still in your backpack?~
+==BSandr25~Let us sit over there and share a glass or two. And not talk anymore.~DO~ StartCutSceneMode() Wait(2) FadeToColor([30.0],0) Wait(4) FadeFromColor([30.0],0) Wait(2) EndCutSceneMode() ~ EXIT
