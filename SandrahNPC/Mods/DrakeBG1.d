6a8ee3f53769aaddd05ff94d74646128c7683066
RoxanneSHS <cwillenbockel@aol.com>
2017-10-30 10:25:53 +0100

    editorial

--- SandrahNPC/Mods/DrakeBG1.d
+++ SandrahNPC/Mods/DrakeBG1.d
@@ -1,5 +1,5 @@
 I_C_T C0Drake 15  SanMeetC0Drake
 ==CVSandrJ IF~InParty("CVSandr")~THEN~And in those two weeks he did not do you the favour to step into this tavern? ~
 ==C0Drake IF~InParty("CVSandr")~THEN~You...shouldn't a priestess of Mystra be the first one to wish a Cyricist necromancer dead?  ~
-==CVSandrJ IF~InParty("CVSandr")~THEN~That is correct.~
+==CVSandrJ IF~InParty("CVSandr")~THEN~This is correct.~
  END
