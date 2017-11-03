3b57d0b44f0852e6a7280cbdbfb349ad8d9c0976
RoxanneSHS <cwillenbockel@aol.com>
2017-11-03 18:23:02 +0100

    Black Pits corrected

--- SandrahNPC/Mods/Blackpit.d
+++ SandrahNPC/Mods/Blackpit.d
@@ -12,6 +12,74 @@ SAY~Do not test my patience, mortal.~
 IF~~THEN REPLY~Najim, I'm ready to depart.~DO~SetGlobal("BLACK_PITS_VICTORY","GLOBAL",1) ClearAllActions() StartCutScene("CVCut305")~EXIT
 IF~~THEN REPLY~I'll get back to you in a little while.~EXIT
 END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("BATTLE_COMPLETE","GLOBAL",2)
+Global("T3_B2","GLOBAL",2) ~ THEN BEGIN san13
+SAY ~No, my master. I obey your every command. If anyone counsels them, I assure you it is not I.~
+IF~~THEN DO~ GiveGoldForce(1000)
+SetGlobal("T3_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",302)
+SetCutSceneLite(TRUE)
+StartCutScene("CUTBP03")
+Wait(2)
+Deactivate("BPNAJIM")~EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("TIER","GLOBAL",3) GlobalLT("BPNAJIM_DIALOG","GLOBAL",30) ~ THEN BEGIN san10
+SAY ~Never have I witnessed a mortal fret Baeloth so. I must confess to taking a certain pleasure in witnessing his dismay. Take this gift. I have added something extra. Perhaps you will have a better chance than Baeloth intends.~ [BPL304]
+IF ~~ THEN REPLY  ~What is Baeloth planning for me? What will happen if I defeat his challenges?~DO ~SetGlobal("BPNAJIM_DIALOG","GLOBAL",30)
+GiveGoldForce(1000)
+~ GOTO 11
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("TIER","GLOBAL",2) GlobalLT("BPNAJIM_DIALOG","GLOBAL",20) ~ THEN BEGIN san5
+SAY ~Impressive, mortal. Your feats in the arena have become... a frequent topic of conversation. Perhaps you will set a new record for longest surviving combatant.~ [BPL205]
+IF ~~ THEN REPLY ~Is there nothing you can do to help us escape?~ DO ~SetGlobal("BPNAJIM_DIALOG","GLOBAL",20)~ GOTO san6
+IF ~~ THEN REPLY ~I will conquer whatever obstacle that is placed in front of me. I will scale a mountain of slain enemies to escape this dungeon!~ DO ~SetGlobal("BPNAJIM_DIALOG","GLOBAL",20) ~ GOTO san7
+IF ~~ THEN REPLY ~Perhaps I will, Najim.~ DO ~SetGlobal("BPNAJIM_DIALOG","GLOBAL",20)
+~ GOTO san8
+END
+
+IF ~~ THEN BEGIN san6
+SAY ~No. Baeloth has tasked me with giving you this reward for your outstanding performance. Take it, and prepare for your next battle.~ [BPL206]
+IF ~~ THEN DO ~ClearAllActions()
+StartCutSceneMode()
+GiveGoldForce(1000)
+CreateVisualEffectObject("spdimndr","BPNAJIM")
+PlaySound("EFF_M13")
+Wait(4)
+Deactivate("BPNAJIM")
+EndCutSceneMode()
+~ EXIT
+END
+
+IF ~~ THEN BEGIN san7
+SAY ~You overestimate your abilities, <RACE>. Baeloth has tasked me with giving you this reward for your outstanding performance. Take it, and prepare for your next battle.~ [BPL207]
+IF ~~ THEN DO ~ClearAllActions()
+StartCutSceneMode()
+GiveGoldForce(1000)
+CreateVisualEffectObject("spdimndr","BPNAJIM")
+PlaySound("EFF_M13")
+Wait(4)
+Deactivate("BPNAJIM")
+EndCutSceneMode()
+~ EXIT
+END
+
+IF ~~ THEN BEGIN san8
+SAY ~Baeloth has tasked me with giving you this reward for your outstanding performance. Take it, and prepare for your next battle.~ [BPL208]
+IF ~~ THEN DO ~ClearAllActions()
+StartCutSceneMode()
+GiveGoldForce(1000)
+CreateVisualEffectObject("spdimndr","BPNAJIM")
+PlaySound("EFF_M13")
+Wait(4)
+Deactivate("BPNAJIM")
+EndCutSceneMode()
+~ EXIT
+END
+
 END
 
 BEGIN CVWDDWA
@@ -105,3 +173,338 @@ END
 CHAIN
 IF~~THEN CVSandrJ BPWDE2
 ~I think that Najim will be grateful enough for the freedom he gains through Baeloth's defeat. There is no reason for him not to let us go. I am confident.~EXIT
+
+APPEND BPBaelot
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T0_B1","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",1) ~ THEN BEGIN san12
+SAY ~That certainly could have gone worse. You've proven you can stamp out the weakest of opponents. Now let's see how you do against some who have eaten in the last tenday!~ [BPT0B102]
+  IF ~~ THEN DO ~GiveGoldForce(50)
+SetGlobal("T0_B1","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",1)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2)  Global("T0_B2","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",2) ~ THEN BEGIN san14
+SAY ~Well done, my precious pugilists! You've certainly proven that I picked well when I picked you to perish in the Black Pits!~ [BPT0B202]
+  IF ~~ THEN DO ~GiveGoldForce(50)
+SetGlobal("T0_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",2)
+SetGlobal("TIER","GLOBAL",1)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2)Global("T1_B1","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",101) ~ THEN BEGIN san16
+SAY ~Congratulations, you have slain a bunch of boozy bumblers. Najim shall ensure that you are awarded appropriately.~ [BPT1B102]
+IF ~~ THEN DO ~GiveGoldForce(75)
+SetGlobal("T1_B1","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",101)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B2","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",102) ~ THEN BEGIN san19
+SAY ~Goodness! The look on their faces when they realized they would never again see those disgusting dens they call home. I think that deserves a bonus.~ [BPT1B202]
+IF ~~ THEN DO ~GiveGoldForce(200)
+SetGlobal("T1_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",102)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B2","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",102) GlobalGT("LAST_BATTLE","GLOBAL",102) ~ THEN BEGIN san20
+SAY ~Goodness! The look on their faces when they realized they would never again see those disgusting dens they call home. I think that deserves a bonus.~ [BPT1B202]
+IF ~~ THEN DO ~GiveGoldForce(100)
+SetGlobal("T1_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B3","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",103) ~ THEN BEGIN san22
+SAY ~Uncanny! You seem to have a knack for killing things that are already dead. If only you displayed a little more flair and enthusiasm for your work.~ [BPT1B302]
+IF ~~ THEN DO ~GiveGoldForce(225)
+SetGlobal("T1_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",103)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B3","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",103) GlobalGT("LAST_BATTLE","GLOBAL",103) ~ THEN BEGIN san23
+SAY ~Uncanny! You seem to have a knack for killing things that are already dead. If only you displayed a little more flair and enthusiasm for your work.~ [BPT1B302]
+IF ~~ THEN DO ~GiveGoldForce(115)
+SetGlobal("T1_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B4","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",104) ~ THEN BEGIN san25
+SAY  ~Well, you may be able to take a hit, but you certainly won't make one fighting such dull brutes. I must provide a more engaging foe for our next fracas!~ [BPT1B402]
+IF ~~ THEN DO ~GiveGoldForce(250)
+SetGlobal("T1_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",104)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2)Global("T1_B4","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",104) GlobalGT("LAST_BATTLE","GLOBAL",104)~ THEN BEGIN san26
+SAY ~Well, you may be able to take a hit, but you certainly won't make one fighting such dull brutes. I must provide a more engaging foe for our next fracas!~ [BPT1B402]
+IF ~~ THEN DO ~GiveGoldForce(125)
+SetGlobal("T1_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T1_B5","GLOBAL",2)GlobalLT("LAST_BATTLE","GLOBAL",105)~ THEN BEGIN san28
+SAY ~Inconceivable! Get out of here, and take your reward with you.~ [BPT1B502]
+IF ~~ THEN DO ~SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",105)
+SetGlobal("T1_B5","GLOBAL",-1)
+SetGlobal("TIER","GLOBAL",2)
+GiveGoldForce(300)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2)Global("T1_B5","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",105)GlobalGT("LAST_BATTLE","GLOBAL",105) ~ THEN BEGIN san29
+SAY ~Inconceivable! Get out of here, and take your reward with you.~ [BPT1B502]
+IF ~~ THEN DO ~SetGlobal("T1_B5","GLOBAL",-1)
+GiveGoldForce(150)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B1","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",201) ~ THEN BEGIN san35
+SAY ~After that last contest, I don't know whether to call you a gladiator or an exterminator.~ [BPT2B102]
+IF ~~ THEN DO ~GiveGoldForce(400)
+SetGlobal("T2_B1","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",201)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B1","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",201) GlobalGT("LAST_BATTLE","GLOBAL",201) ~ THEN BEGIN san36
+SAY  ~After that last contest, I don't know whether to call you a gladiator or an exterminator.~ [BPT2B102]
+IF ~~ THEN DO ~GiveGoldForce(200)
+SetGlobal("T2_B1","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B2","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",202)~ THEN BEGIN san38
+SAY ~Again, you prove yourself a skilled swatter of insects. Perhaps what you need are some enemies with real backbone!~ [BPT2B202]
+IF ~~ THEN DO ~GiveGoldForce(450)
+SetGlobal("T2_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",202)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B2","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",202)GlobalGT("LAST_BATTLE","GLOBAL",202) ~ THEN BEGIN san39
+SAY ~Again, you prove yourself a skilled swatter of insects. Perhaps what you need are some enemies with real backbone!~ [BPT2B202]
+  IF ~~ THEN DO ~GiveGoldForce(225)
+SetGlobal("T2_B2","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B3","GLOBAL",2)GlobalLT("LAST_BATTLE","GLOBAL",203) ~ THEN BEGIN san41
+SAY ~Hmm... I didn't think you'd sidestep all those traps I instructed them to set up. You should be a pile of constituent body parts right now! Bah, take your reward and choke on it.~ [BPT2B302]
+IF ~~ THEN DO ~GiveGoldForce(500)
+SetGlobal("T2_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",203)
+TriggerActivation("GREASETRAP",FALSE)
+TriggerActivation("ARROWTRAP1",FALSE)
+TriggerActivation("ARROWTRAP2",FALSE)
+TriggerActivation("ARROWTRAP3",FALSE)
+TriggerActivation("ARROWTRAP4",FALSE)
+TriggerActivation("ARROWTRAP5",FALSE)
+TriggerActivation("ARROWTRAP6",FALSE)
+TriggerActivation("STINKYTRAP",FALSE)
+TriggerActivation("JERKTRAP",FALSE)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B3","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",203) GlobalGT("LAST_BATTLE","GLOBAL",203) ~ THEN BEGIN san42
+SAY ~Hmm... I didn't think you'd sidestep all those traps I instructed them to set up. You should be a pile of constituent body parts right now! Bah, take your reward and choke on it.~ [BPT2B302]
+IF ~~ THEN DO ~GiveGoldForce(250)
+SetGlobal("T2_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+TriggerActivation("GREASETRAP",FALSE)
+TriggerActivation("ARROWTRAP1",FALSE)
+TriggerActivation("ARROWTRAP2",FALSE)
+TriggerActivation("ARROWTRAP3",FALSE)
+TriggerActivation("ARROWTRAP4",FALSE)
+TriggerActivation("ARROWTRAP5",FALSE)
+TriggerActivation("ARROWTRAP6",FALSE)
+TriggerActivation("STINKYTRAP",FALSE)
+TriggerActivation("JERKTRAP",FALSE)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B4","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",204)~ THEN BEGIN san44
+SAY ~That was hardly fair, routing your rival's reinforcements. But you have given me a great idea. What this contest needs are some fresh faces, and I have just the plan to provide some.~ [BPT2B402]
+IF ~~ THEN DO ~GiveGoldForce(550)
+SetGlobal("T2_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",204)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B4","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",204) GlobalGT("LAST_BATTLE","GLOBAL",204) ~ THEN BEGIN san45
+SAY ~That was hardly fair, routing your rival's reinforcements. But you have given me a great idea. What this contest needs are some fresh faces, and I have just the plan to provide some.~ [BPT2B402]
+IF ~~ THEN DO ~GiveGoldForce(275)
+SetGlobal("T2_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T2_B5","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",205) GlobalGT("LAST_BATTLE","GLOBAL",205)~ THEN BEGIN san48
+SAY ~Get. Out. Get out! GET OUT OF MY SIGHT BEFORE I BURN YOUR EYES FROM THEIR SOCKETS!~ [BPT2B502]
+IF ~~ THEN REPLY~What happened to all the alliteration? I hope we haven't broken Baeloth.~ DO ~GiveGoldForce(300)
+SetGlobal("T2_B5","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("TIER","GLOBAL",3)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) !Global("START_BATTLE","GLOBAL",0)Global("T3_B1","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",301) ~ THEN BEGIN san54
+SAY ~Oh, this is no fun! Your neck remains unscathed and your pantaloons are rapidly drying! What a waste of good slimes...~ [BPT3B104]
+IF ~~ THEN DO ~GiveGoldForce(800)
+SetGlobal("T3_B1","GLOBAL",-1)
+SetGlobal("LAST_BATTLE","GLOBAL",301)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B1","GLOBAL",2) OR(2)
+Global("LAST_BATTLE","GLOBAL",301) GlobalGT("LAST_BATTLE","GLOBAL",301)~ THEN BEGIN san56
+SAY ~Oh, this is no fun! Your neck remains unscathed and your pantaloons are rapidly drying! What a waste of good slimes...~ [BPT3B104]
+IF ~~ THEN DO ~GiveGoldForce(400)
+SetGlobal("T3_B1","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B2","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",302) ~ THEN BEGIN san59
+SAY~What a despicable display of enchanted weapons. Hm, it's almost as if someone gave them advice. Was it you, Najim?~ [BPT3B202]
+IF ~~ THEN DO ~SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+CreateVisualEffect("spdimndr",[591.503])
+PlaySound("EFF_M13")
+Wait(4)
+Activate("BPNAJIM")
+ActionOverride("BPNAJIM",StartDialogNoSet(Player1))
+~ EXTERN BPNAJIM san13
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B2","GLOBAL",2)OR(2) Global("LAST_BATTLE","GLOBAL",302)GlobalGT("LAST_BATTLE","GLOBAL",302) ~ THEN BEGIN san60
+SAY~What a despicable display of enchanted weapons. Hm, it's almost as if someone gave them advice. Was it you, Najim?~ [BPT3B202]
+IF ~~ THEN DO ~SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+CreateVisualEffect("spdimndr",[591.503])
+PlaySound("EFF_M13")
+Wait(4)
+Activate("BPNAJIM")
+ActionOverride("BPNAJIM",StartDialogNoSet(Player1))
+~ EXTERN BPNAJIM san13
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B3","GLOBAL",2)GlobalLT("LAST_BATTLE","GLOBAL",303)~ THEN BEGIN san62
+SAY ~Confound it! Once more, you have anticipated my surprise! How does this keep happening?~ [BPT3B302]
+IF ~~ THEN DO ~GiveGoldForce(1200)
+SetGlobal("T3_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",303)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B3","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",303) GlobalGT("LAST_BATTLE","GLOBAL",303)~ THEN BEGIN san63
+SAY ~Confound it! Once more, you have anticipated my surprise! How does this keep happening?~ [BPT3B302]
+  IF ~~ THEN DO ~GiveGoldForce(600)
+SetGlobal("T3_B3","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B4","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",304) ~ THEN BEGIN san65
+SAY  ~Heh huh... it appears I have miscalculated once again. I grow tired of this game. Begone, <RACE>! You are beginning to bother me.~ [BPT3B402]
+  IF ~~ THEN DO ~GiveGoldForce(1400)
+SetGlobal("T3_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetGlobal("LAST_BATTLE","GLOBAL",304)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2) Global("T3_B4","GLOBAL",2) OR(2) Global("LAST_BATTLE","GLOBAL",304)GlobalGT("LAST_BATTLE","GLOBAL",304)~ THEN BEGIN san66
+SAY ~Heh huh... it appears I have miscalculated once again. I grow tired of this game. Begone, <RACE>! You are beginning to bother me.~ [BPT3B402]
+IF ~~ THEN DO ~GiveGoldForce(700)
+SetGlobal("T3_B4","GLOBAL",-1)
+SetGlobal("START_BATTLE","GLOBAL",0)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+END
+
+END
+
+CHAIN
+IF WEIGHT #-3 ~ GlobalGT("SanPitsWD","Global",2)Global("T3_B1","GLOBAL",2) GlobalLT("LAST_BATTLE","GLOBAL",301) ~THEN BPNajim SanN12
+~I am afraid it is "rock crushes lizard," master.~[BPT3D103]
+==BPBaelot~Oh, this is no fun! Your neck remains unscathed and your pantaloons are rapidly drying! What a waste of good slimes...~ [BPT3B104] 
+DO~GiveGoldForce(800)
+SetGlobal("T3_B1","GLOBAL",-1)
+SetGlobal("LAST_BATTLE","GLOBAL",301)
+SetCutSceneLite(TRUE)
+StartCutSceneEx("CUTBP03",FALSE)
+~ EXIT
+
