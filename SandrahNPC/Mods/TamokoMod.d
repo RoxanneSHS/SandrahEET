ba1193f65326d8c60f69beb108fbc60c37475727
RoxanneSHS <cwillenbockel@aol.com>
2018-04-24 12:24:20 +0200

    Tamoko crossmod

---
+++ SandrahNPC/Mods/TamokoMod.d
@@ -0,0 +1,10 @@
+
+CHAIN
+IF~Global("SanHealTamok","LOCALS",1)~THEN TamokoJ SanHeal
+~By Bane's black hand! I need none of your Mystra spells for healing, priestess!~
+DO~SetGlobal("SanHealTamok","LOCALS",2)~
+==BSandr~Well, dead Bane cannot give you the power for your spells any more. Who is it, Iyachtu Xvim, Bane's son?~
+==TamokoJ~That's not your concern, I won't talk to a slave of (spit) Mystra about this.~
+==BSandr~Had Sarevok succeeded and become God of Murder, that might have provided you with a new deity to follow.~
+==TamokoJ~Shall I kill you or ignore you, girl. I have not decided yet, maybe <CHARNAME> gets us rid of you soon enough. It will not take long for <PRO_HIMHER> to realise that <PRO_HESHE> can't advance with you standing in the path to the Throne of Bhaal.~
+==BSandr~Ha, so that is what you expect to gain from following <CHARNAME>. I was suspecting something like that.~EXIT
