ce4a1a261102cfa9733ec0f5e33f2d759389f0c4
RoxanneSHS <cwillenbockel@aol.com>
2018-09-23 23:48:39 +0200

    File not used

--- SandrahNPC/D/Sanpre.d
+++ SandrahNPC/D/Sanpre.d
@@ -1,4 +1,3 @@
-BEGIN Gorion2
 BEGIN Sanpre
 
 CHAIN
@@ -6,7 +5,7 @@ IF ~NumTimesTalkedTo(0)~ THEN Sanpre JT1
 ~...does not know I am here.~
 == Gorion ~He should have known you would inherit his adventurous ways. But this situation here is beyond your skills yet, child, you need to stay away from us.~
 == Sanpre ~But I know you need help, I have seen it in his crystal shard...~
-== Gorion ~ Enough, young lady, I don't need the details of your infringement. Yes, you are talented and learned much from him, but you have no idea of the powers that are at work here. ~
+== Gorion ~ Enough, young lady, I don't need the details of your infringement. Yes, you are talented and learned much from him, but you have no idea of the powers that are at work here. ~DO~SetGlobal("Sprite_is_deadoublek","Global",1)~
 == Sanpre ~ But I.... ~
 == Gorion ~ NO, enough words, you have to leave immediately!~
 == Sanpre ~(You see a beautiful girl of your age turn away from your foster father with an angry face. She hardly seems to notice you as she rushes by.)~ 
