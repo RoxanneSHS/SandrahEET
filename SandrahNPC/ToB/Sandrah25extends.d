7f0a649858e2b07f830710fb5235fc709d9c8d24
RoxanneSHS <cwillenbockel@aol.com>
2019-06-04 11:31:56 +0200

    No need for Ascension

--- SandrahNPC/ToB/Sandrah25extends.d
+++ SandrahNPC/ToB/Sandrah25extends.d
@@ -242,29 +242,6 @@ I_C_T OHNSzass 1 SanOhSzassR
 ==OHNSzass IF~InParty("CVSandr")~THEN~Back to you, Neera and <CHARNAME>.~
 END
 
-//Melissan Showdown
-INTERJECT_COPY_TRANS Finmel01 27 SanOddElement1
-==CVSan25J IF ~InParty("CVSandr") OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~You never had a chance to succeed - neither for your own ambitions nor for the case you had really followed Bhaal's orders.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~YOU!! Priestess, you and your goddess have dared to intervene again and again. You should not be here at all but dead since long already. But never mind, this will all be corrected now.~
-==CVSan25J IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~With the essence of Qilandrha missing from your equations and the power of Mystra against you, you will fail.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~Who by the Nine Hells is Qilandrha?~
-==CVQidr IF~InParty("CVSandr") InMyArea("CVQidr")~THEN~I am Qilandrha - born to Khalindra of Mystra's bloodline, sired by Bhaal.~
-==CVSan25J IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~Tragic if it were not so laughable, Amelyssan. Did Bhaal not tell you about the one he spawned to end Mystra's line? The one who has more of the essence than the Five or even <CHARNAME>.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~A silly bluff - if such a power existed I would know it...it is impossible...Unless - the spawn failed and you are Mystra's descendent who survived...~
-==CVSan25J IF ~InParty("CVSandr")!InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~It is even worse for you, as Qilandrha has defeated the calling of her blood and turned it into a power against Bhaal and yourself.~
-==CVQidr IF ~InParty("CVSandr")InMyArea("CVQidr") ~THEN~It is even worse for you, as I have defeated the calling of my blood and turned it into a power against Bhaal and yourself.~END
-
-INTERJECT_COPY_TRANS Finmel01 25 SanOddElement2
-==CVSan25J IF ~InParty("CVSandr") OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~You never had a chance to succeed - neither for your own ambitions nor for the case you had really followed Bhaal's orders.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~YOU!! Priestess, you and your goddess have dared to intervene again and again. You should not be here at all but dead since long already. But never mind, this will all be corrected now.~
-==CVSan25J IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~With the essence of Qilandrha missing from your equations and the power of Mystra against you, you will fail.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~Who by the Nine Hells is Qilandrha?~
-==CVQidr IF~InParty("CVSandr") InMyArea("CVQidr")~THEN~I am Qilandrha - born to Khalindra of Mystra's bloodline, sired by Bhaal.~
-==CVSan25J IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~Tragic if it were not so laughable, Amelyssan. Did Bhaal not tell you about the one he spawned to end Mystra's line? The one who has more of the essence than the Five or even <CHARNAME>.~
-==Finmel01 IF ~InParty("CVSandr")OR(2) InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~A silly bluff - if such a power existed I would know it...it is impossible...Unless - the spawn failed and you are Mystra's descendent who survived...~
-==CVSan25J IF ~InParty("CVSandr")!InMyArea("CVQidr") PartyHasItem("CVQCall")~THEN~It is even worse for you, as Qilandrha has defeated the calling of her blood and turned it into a power against Bhaal and yourself.~
-==CVQidr IF ~InParty("CVSandr")InMyArea("CVQidr") ~THEN~It is even worse for you, as I have defeated the calling of my blood and turned it into a power against Bhaal and yourself.~END
-
 INTERJECT_COPY_TRANS Finsol01 14 SanQidrEssence1
 ==CVSan25J IF ~InParty("CVSandr") !InMyArea("CVQidr2") ~THEN~What will happen to my sister and her essence that she uses in the name of Eilistraeee and Mystra and has no intention to apply in an evil way.~
 ==Finsol01 IF ~InParty("CVSandr") !InMyArea("CVQidr2") ~THEN~The odd element, hidden by those mortals from all...even from the celestials except for your goddess. What would you do about it had you the right to decide, Sandrah?~
