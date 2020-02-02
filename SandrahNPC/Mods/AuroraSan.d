c3e6bc62852521eebc18d99786a09a540e547a57
RoxanneSHS <cwillenbockel@aol.com>
2020-02-02 07:52:05 +0100

    Cedric improved

--- SandrahNPC/Mods/AuroraSan.d
+++ SandrahNPC/Mods/AuroraSan.d
@@ -3,7 +3,7 @@ BEGIN CVCedri
 IF ~Global("KarMovedIn","LOCALS",0)~THEN BEGIN KarMovedIn1
 SAY ~Hello, I know you are friends of Henning the shop owner here. I was so sorry when I heard what has happened to our mutual friend. I am Cedric.~
 IF~~THEN REPLY ~Have you taken the chance to occupy his shop in the meantime, you little misfit? ~GOTO KarMovedInRude
-IF~~THEN REPLY ~You are his friend as well, it is good that somebody takes care of his business in the meantime. We hope to find him soon.~GOTO KarMovedInFr
+IF~~THEN REPLY ~You are his friend as well, it is good that somebody takes care of his business in the meantime. ~GOTO KarMovedInFr
 END
 
 IF ~~THEN BEGIN KarMovedInRude
@@ -13,7 +13,8 @@ END
 
 IF ~~THEN BEGIN KarMovedInFr
 SAY ~I am so shocked by his kidnapping. I am his friend since I came to Nashkel. We are both in the trade but our wares are different and we can profit side by side. He has often already offered me a space in his shop, but I had not accepted his generosity. But I have taken care here when he was away in the past to acquire new wares. I think he would agree.~
-IF~~THEN REPLY ~It is good to see that care is been taken here. We will tell him about your help as soon as we find him. Hopefully that will be soon already.~DO ~SetGlobal("KarMovedIn","LOCALS",1)~GOTO KarShop
+IF~GlobalLT("OrcPrison","GLOBAL",12)~THEN REPLY ~It is good to see that care is been taken here. We will tell him about your help as soon as we find him. Hopefully that will be soon already.~DO ~SetGlobal("KarMovedIn","LOCALS",1)~GOTO KarShop
+IF~GlobalGT("OrcPrison","GLOBAL",11)~THEN REPLY ~It is good to see that care is been taken here. We will tell him about your help when we see him again. He is safe at Waterdeep for now.~DO ~SetGlobal("KarMovedIn","LOCALS",1)~GOTO KarShop
 END
 
 IF~Global("KarMovedIn","LOCALS",1)~THEN BEGIN KarShop
