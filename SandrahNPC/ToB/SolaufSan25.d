098033005d548d493bc8e2a68f1240f93fa30577
RoxanneSHS <cwillenbockel@aol.com>
2021-03-27 16:29:32 +0100

    ToB Improvement

--- SandrahNPC/ToB/SolaufSan25.d
+++ SandrahNPC/ToB/SolaufSan25.d
@@ -2,40 +2,40 @@ CHAIN
 IF~Global("SanSola","GLOBAL",17)~THEN BSandr25 SanSolGods
 @0
 DO~SetGlobal("SanSola","GLOBAL",18)RealSetGlobalTimer("SanSolaT","LOCALS",3600)~
-==BSOLA25@1
+==BSola@1
 ==BSandr25@2
-==BSOLA25@3
+==BSola@3
 ==BSandr25@4
-==BSOLA25@5
+==BSola@5
 ==BSandr25@6
-==BSOLA25@7
+==BSola@7
 ==BSandr25@8
-==BSOLA25@9
+==BSola@9
 ==BSandr25@10EXIT
 
 CHAIN
 IF~Global("SanSola","GLOBAL",19)~THEN BSandr25 SanSolResp
 @11
 DO~SetGlobal("SanSola","GLOBAL",20)RealSetGlobalTimer("SanSolaT","LOCALS",1600)~
-==BSOLA25@12
+==BSola@12
 ==BSandr25@13
-==BSOLA25@14
+==BSola@14
 ==BSandr25@15
-==BSOLA25@16
+==BSola@16
 ==BSandr25@17
-==BSOLA25@18
+==BSola@18
 ==BSandr25@19
-==BSOLA25@20
+==BSola@20
 ==BSandr25@21EXIT
 
 CHAIN
 IF~Global("SanSolUM1","GLOBAL",2)~THEN BSandr25 SanSolUM
 @22
 DO~SetGlobal("SanSolUM1","GLOBAL",3)~
-==BSOLA25@23
+==BSola@23
 =@24
 ==BSandr25@25
-==BSOLA25@26EXIT
+==BSola@26EXIT
 
 EXTEND_BOTTOM SOLA 343
 IF~InParty("CVSandr") Global("Waiting","LOCALS",0)!InParty(Myself) Global("SolaUmo5","LOCALS",1)~THEN REPLY@27GOTO Qidr1
